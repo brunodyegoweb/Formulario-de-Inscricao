76af16bb2f5bf672e8a2f6ba12964dfe26e67127
Bruno Dyego Silveira da Silva <bruno.ufra@yahoo.com.br>
2023-07-18 21:21:06 -0300

    Update index and js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let input = document.querySelector('.input')
 let button = document.querySelector('.input.-btn')
+let email = document.querySelector('.email')
 let span = document.querySelector('.spn')
 
 
 function enviar() {
     if(input.value === "") {
         alert("[ERRO] Preencha o campoe corretamente")
+        email.innerHTML = "Requered email valided"
         return false
     }else {
         alert('ok')
